fbd546cb5db82e87c9ac4677847a5a1390fb38b2
jblneely <bowman.neely@gmail.com>
2017-05-12 10:50:13 -0700

    added migration for Facebook and updated users model

--- migrations/20170501202641-create-user.js
+++ migrations/20170501202641-create-user.js
@@ -1,33 +1,35 @@
 'use strict';
 module.exports = {
-  up: function(queryInterface, Sequelize) {
-    return queryInterface.createTable('users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      firstName: {
-        type: Sequelize.STRING
-      },
-      lastName: {
-        type: Sequelize.STRING
-      },
-      email: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: function(queryInterface, Sequelize) {
-    return queryInterface.dropTable('users');
-  }
+    up: function(queryInterface, Sequelize) {
+        return queryInterface.createTable('users', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            firstName: {
+                type: Sequelize.STRING
+            },
+            lastName: {
+                type: Sequelize.STRING
+            },
+            email: {
+                type: Sequelize.STRING
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            facebookId: DataTypes.STRING,
+            facebookToken: DataTypes.STRING
+        });
+    },
+    down: function(queryInterface, Sequelize) {
+        return queryInterface.dropTable('users');
+    }
 };
